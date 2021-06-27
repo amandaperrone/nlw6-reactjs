f51b813740c7d1e991b1a9900e6760740bdde558
amandaperrone <amanda_degrande@yahoo.com.br>
2021-06-27 16:09:11 -0300

    aula 04

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 import { AuthContextProvider } from './contexts/AuthContext';
 
@@ -16,6 +17,8 @@ function App() {
           <Route path="/" exact component={Home} /> {/* exact => tem que ser literalmente esse caminho */}
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>   
     </BrowserRouter>
